62010904791e0c3c7739cfa22540db1481657092
Zeynel <zeynelbasaran@hotmail.com>
2025-02-24 23:56:33 +0300

    Premium sayfası tamamlandı

--- src/pages/abonelik/abonelik.jsx
+++ src/pages/abonelik/abonelik.jsx
@@ -1,13 +1,28 @@
-import Navbar from "../navbar/navbar"
+import Navbar from "../navbar/navbar";
+import PremiumCard from "./components/premiumCard";
 
-
-const abonelik = () => {
+const Abonelik = () => {
   return (
-    <main className="bg-bg-main container flex">
-        <Navbar/>
-        abonelik
-  </main>
-  )
-}
+    <main className="bg-bg-main container flex gap-5 p-12">
+      <Navbar />
+      <aside className="w-[1290px]">
+        <section className="text-center mt-5">
+          <h5 className="text-3xl font-bold">Premium Dünyasına </h5>
+          <p
+            className="font-light text-4xl mt-3
+"
+          >
+            Hoş Geldin
+          </p>
+        </section>
+        
+        <PremiumCard />
 
-export default abonelik
+        <div className="text-sm text-center font-semibold text-basitplan mt-12
+">Hangi premium seçenek sana daha uygun ?</div>
+      </aside>
+    </main>
+  );
+};
+
+export default Abonelik;
